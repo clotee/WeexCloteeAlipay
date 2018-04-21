4055b95bcbb40c2f746688ca35d2bdc530ade25d
clotee <clotee@foxmail.com>
2018-04-21 18:52:27 +0800

    Create file

---
+++ Pods/Target Support Files/NatAlipay/NatAlipay-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NatAlipay : NSObject
+@end
+@implementation PodsDummy_NatAlipay
+@end
---
+++ Pods/Target Support Files/Pods-WeexCloteeAlipay/Pods-WeexCloteeAlipay-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeexCloteeAlipay : NSObject
+@end
+@implementation PodsDummy_Pods_WeexCloteeAlipay
+@end
